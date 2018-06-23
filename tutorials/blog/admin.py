271744781bc847daf56ceab2d3879ca14a4922e5
lamuda114 <frontier217@gmail.com>
2018-06-23 13:14:13 +0900

    no message

--- tutorials/blog/admin.py
+++ tutorials/blog/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Post
 
 # Register your models here.
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'is_public', 'created_at']
+    list_display_links = ['title']    #타이틀에 링크 달기
+    list_editable = ['is_public']    #체크박스로 변환
+
+admin.site.register(Post, PostAdmin)
--- tutorials/blog/models.py
+++ tutorials/blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    is_public = models.BooleanField(default=False)    #공개여부 선택
+    created_at = models.DateTimeField(auto_now_add=True)    #글 올린 시간
+    updated_at = models.DateTimeField(auto_now=True)    #글 수정한 시간
+
+    def __str__(self):
+        return self.title    #리스트에 제목 출력
