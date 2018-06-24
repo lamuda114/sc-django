4177615c94421dea9eaa31a810b3662afdb06867
lamuda114 <frontier217@gmail.com>
2018-06-24 09:16:20 +0900

    no message

--- tutorials/blog/models.py
+++ tutorials/blog/models.py
@@ -1,13 +1,22 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
     photo = models.ImageField()    #이미지 필드 생성
+    lnglat = models.CharField(max_length=40, blank=True)
     is_public = models.BooleanField(default=False)    #공개여부 선택
     created_at = models.DateTimeField(auto_now_add=True)    #글 올린 시간
     updated_at = models.DateTimeField(auto_now=True)    #글 수정한 시간
 
     def __str__(self):
         return self.title    #리스트에 제목 출력
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
--- tutorials/blog/views.py
+++ tutorials/blog/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import get_object_or_404, render
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import get_object_or_404, redirect, render
 from .models import Post
+from .forms import CommentForm
 # Create your views here.
 
 def index(request):
@@ -12,4 +14,22 @@ def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {
         'post': post,
+    })
+
+@login_required
+def comment_new(request, post_pk):
+    post = get_object_or_404(Post, pk=post_pk)
+
+    if request.method == 'POST':
+        form = CommentForm(request.POST, request.FILES)
+        if form.is_balid():
+            comment = form.save(comment=False)
+            comment.post = post
+            comment.author = request.user
+            comment.save()
+            return redirect('blog:post_detail', post_pk)
+    else:
+        form = CommentForm()
+    return render(request, 'blog/comment_form.html', {
+        'form': form,
     })
