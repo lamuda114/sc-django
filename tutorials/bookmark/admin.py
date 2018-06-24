829b9d0ce541accd63ecae830c06b26eb56b5b48
lamuda114 <frontier217@gmail.com>
2018-06-24 11:49:06 +0900

    모델을 admin 에 등록

--- tutorials/bookmark/admin.py
+++ tutorials/bookmark/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Bookmark
 
 # Register your models here.
+class BookmarkAdmin(admin.ModelAdmin):
+    list_display = ['title', 'url']
+    list_display_links = ['url']
+admin.site.register(Bookmark, BookmarkAdmin)
