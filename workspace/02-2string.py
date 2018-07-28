5ce502f16d313d5a078a5a6140dae25e5e8b8d4d
lamuda114 <frontier217@gmail.com>
2018-07-29 06:25:50 +0900

    workspace 폴더추가

---
+++ workspace/02-2string.py
@@ -0,0 +1,15 @@
+food = "Python's favorite food is perl"
+print(food)
+
+str1 = '''Life is too short
+You need Python'''
+print(str1)
+print('='*50)
+print('My Program')
+print('='*50)
+i = len(input("숫자를 입력하세요: "))
+y = '1'
+while i > 1:
+    i -= 1
+    y += '0'
+print('자릿수는{}자리수입니다' . format(y))
