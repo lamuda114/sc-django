9e213260f53ae6ddb8348fa7672d7557c8bbb77f
bonda77 <frontier217@gmail.com>
2019-01-12 08:36:03 +0900

    url 123

--- tutorials/blog/urls.py
+++ tutorials/blog/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 
-app_name = 'blog'    #name 전달 하는 방법
+app_name = 'blog'    #name 전달 하는 방법123
 urlpatterns = [
     path('', views.index, name='index'),
     path('<pk>/', views.post_detail, name='post_detail'),
